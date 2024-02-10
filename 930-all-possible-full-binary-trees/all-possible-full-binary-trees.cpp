54f2d1443f084a4d05d40d196e0045cbc545edde
Pratyaksh <90966241+Pratyaksh0610@users.noreply.github.com>
2024-02-10 20:30:07 +0530

    Time: 67 ms (34.92%) | Memory: 36.1 MB (20.04%) - LeetSync

---
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+private:
+    // TreeNode*alpha=NULL;
+    // TreeNode*copy(TreeNode*parent){
+    //     if(parent==NULL){
+    //         return NULL;
+    //     }
+    //     TreeNode*root=new TreeNode();
+    //     root->left=copy(parent->left);
+    //     root->right=copy(parent->right);
+    //     return root;
+    // }
+    vector<TreeNode*> fun(int n) {
+        vector<TreeNode*> v;
+        if (n == 1) {
+            TreeNode* temp = new TreeNode();
+            v.push_back(temp);
+            return v;
+        }
+        n--;
+        for (int i = 1; i < n; i++) {
+            if (i % 2 == 0) {
+                continue;
+            }
+            vector<TreeNode*>l=fun(i);
+            vector<TreeNode*>r=fun(n-i);
+            for(int j=0;j<l.size();j++){
+                for(int k=0;k<r.size();k++){
+                    TreeNode* temp = new TreeNode();
+                    temp->left=l[j];
+                    temp->right=r[k];
+                    v.push_back(temp);
+                }
+            }
+        }
+        return v;
+    }
+
+public:
+    vector<TreeNode*> allPossibleFBT(int n) {
+        vector<TreeNode*> ans;
+        if (n % 2 == 0) {
+            return ans;
+        }
+        return fun(n);
+        // return ans;
+    }
+};
