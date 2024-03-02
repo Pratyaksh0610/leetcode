b1daba81bf46c7ff94d218146989c8097ebe0f60
Pratyaksh <90966241+Pratyaksh0610@users.noreply.github.com>
2024-03-02 20:37:19 +0530

    Time: 41 ms (49.66%) | Memory: 22.7 MB (50.34%) - LeetSync

--- 3209-minimum-number-of-coins-for-fruits/minimum-number-of-coins-for-fruits.cpp
+++ 3209-minimum-number-of-coins-for-fruits/minimum-number-of-coins-for-fruits.cpp
@@ -1,24 +1,28 @@
 class Solution {
     private:
-    int fun(vector<int>&p,int ind,int allow,vector<vector<int>>&dp){
+    int fun(vector<int>&p,int ind,vector<int>&dp){
         if(ind>=p.size()){
             return 0;
         }
-        else if(dp[ind][allow]!=-1){
-            return dp[ind][allow];
+        else if(dp[ind]!=-1){
+            return dp[ind];
         }
-        int take=p[ind]+fun(p,ind+1,ind+1,dp);
-        int ntake=1e8;
-        if(allow>=1){
-            ntake=fun(p,ind+1,allow-1,dp);
+        int ans=INT_MAX;
+        for(int i=ind+1;i<=2*ind+2;i++){
+            ans=min(ans,p[ind]+fun(p,i,dp));
         }
-        return dp[ind][allow]= min(take,ntake);
+        // int take=p[ind]+fun(p,ind+1,ind+1,dp);
+        // int ntake=1e8;
+        // if(allow>=1){
+        //     ntake=fun(p,ind+1,allow-1,dp);
+        // }
+        return dp[ind]= ans;
     }
 public:
     int minimumCoins(vector<int>& p) {
         int n=p.size();
         // vector<int>dp(n,-1);
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return fun(p,0,0,dp);
+        vector<int>dp(n,-1);
+        return fun(p,0,dp);
     }
 };
