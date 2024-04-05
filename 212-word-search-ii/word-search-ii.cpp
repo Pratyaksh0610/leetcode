94e56b835e8afd18c96edd51ff4ed95db09b267e
Pratyaksh <90966241+Pratyaksh0610@users.noreply.github.com>
2024-04-06 00:23:29 +0530

    Time: 2124 ms (8.96%) | Memory: 19.5 MB (47.49%) - LeetSync

---
+++ 212-word-search-ii/word-search-ii.cpp
@@ -0,0 +1,95 @@
+struct Node {
+	Node *links[26];
+	bool flag = false;
+	//checks if the reference trie is present or not
+	bool containKey(char ch) {
+		return (links[ch - 'a'] != NULL);
+	}
+	//creating reference trie
+	void put(char ch, Node *node) {
+		links[ch - 'a'] = node;
+	}
+	//to get the next node for traversal
+	Node *get(char ch) {
+		return links[ch - 'a'];
+	}
+	//setting flag to true at the end of the word
+	void setEnd() {
+		flag = true;
+	}
+	//checking if the word is completed or not
+	bool isEnd() {
+		return flag;
+	}
+};
+class Trie {
+// public:
+public:
+	Node* root;
+	Trie() {
+		//creating new obejct
+		root = new Node();
+	}
+
+	void insert(string word) {
+		//initializing dummy node pointing to root initially
+		Node *node = root;
+		for (int i = 0; i < word.size(); i++) {
+			if (!node->containKey(word[i])) {
+				node->put(word[i], new Node());
+			}
+			//moves to reference trie
+			node = node->get(word[i]);
+		}
+		node->setEnd();
+	}
+
+    void fun(Node*node,int i,int j,vector<vector<bool>>&vis,vector<vector<char>>&b,set<string>&s,string w){
+        int r=b.size(),c=b[0].size();
+        if(i<0||i>=r||j<0||j>=c||vis[i][j]){
+            return;
+        }
+        // Node*node=root;
+        int ind=b[i][j]-'a';
+        if(!node->containKey(b[i][j])){
+            return;
+        }
+        node=node->links[ind];
+        w.push_back(b[i][j]);
+        if(node->isEnd()){
+            s.insert(w);
+        }
+        vis[i][j]=true;
+        int row[]={-1,0,1,0};
+        int col[]={0,-1,0,1};
+        for(int k=0;k<4;k++){
+            fun(node,i+row[k],j+col[k],vis,b,s,w);
+        }
+        vis[i][j]=false;
+        
+    }
+};
+
+class Solution {
+public:
+    vector<string> findWords(vector<vector<char>>& b, vector<string>& w) {
+        int r=b.size(),c=b[0].size(),n=w.size();
+        vector<string>ans;
+        Trie t;
+        for(int i=0;i<n;i++){
+            t.insert(w[i]);
+        }
+        vector<vector<bool>>vis(r,vector<bool>(c,0));
+        set<string>s;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                t.fun(t.root,i,j,vis,b,s,"");
+            }
+        }
+        for(auto it:s){
+            ans.push_back(it);
+        }
+
+        return ans;
+    }
+};
