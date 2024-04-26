4e1c91afefb4b80e302b451647f61b18af453bf4
Pratyaksh <90966241+Pratyaksh0610@users.noreply.github.com>
2024-04-26 07:32:49 +0530

    Time: 37 ms (80.70%) | Memory: 17.9 MB (69.40%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -1,33 +1,40 @@
+#define pi pair<int,int>
+
 class Solution {
-    private:
-    int fun(vector<vector<int>>&g,vector<vector<int>>&dp,int i,int j){
-        int r=g.size(),c=g[0].size();
-        if(i<0||i>=r||j<0||j>=c){
-            return 1e8;
-        }
-        else if(i==r-1){
-            return dp[i][j]= g[i][j];
-        }
-        else if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int val=INT_MAX;
-        for(int k=0;k<c;k++){
-            if(k==j){
-                continue;
-            }
-            val=min(val,fun(g,dp,i+1,k));
-        }
-        return dp[i][j]=val+g[i][j];
-    }
 public:
     int minFallingPathSum(vector<vector<int>>& g) {
         int r=g.size(),c=g[0].size();
-        vector<vector<int>>dp(r,vector<int>(c,-1));
-        int ans=INT_MAX;
+        priority_queue<pi>pq;
+        //bade se chota
+
         for(int j=0;j<c;j++){
-            ans=min(ans,fun(g,dp,0,j));
+            pq.push({g[r-1][j],j});
+            if(pq.size()>2){
+                pq.pop();
+            }
         }
-        return ans;
+        for(int i=r-2;i>=0;i--){
+            //bigger
+            pi m1=pq.top();
+            pq.pop();
+
+            //smaller
+            pi m2=pq.top();
+            pq.pop();
+
+            for(int j=0;j<c;j++){
+                if(j!=m2.second){
+                    pq.push({g[i][j]+m2.first,j});
+                }
+                else{
+                    pq.push({g[i][j]+m1.first,j});
+                }
+                if(pq.size()>2){
+                    pq.pop();
+                }
+            }
+        }
+        pq.pop();
+        return pq.top().first;
     }
 };
