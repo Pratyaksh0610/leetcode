52ee7932eaa9270ead80c126317cff3dbe1a3912
Pratyaksh <90966241+Pratyaksh0610@users.noreply.github.com>
2024-05-23 23:23:23 +0530

    Time: 6 ms (87.67%) | Memory: 16.6 MB (19.91%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -2,29 +2,49 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& m) {
         int r=m.size(),c=m[0].size();
-        vector<bool>prow(r,false),pcol(c,false);
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(m[i][j]==0){
-                    prow[i]=true;
-                    pcol[j]=true;
-                }
+
+        bool rowz=false,colz=false;
+        for(int j=0;j<c;j++){
+            if(m[0][j]==0){
+                rowz=true;
             }
         }
         for(int i=0;i<r;i++){
-            if(prow[i]){
+            if(m[i][0]==0){
+                colz=true;
+            }
+        }
+        for(int i=1;i<r;i++){
+            for(int j=1;j<c;j++){
+                if(m[i][j]==0){
+                    m[0][j]=0;
+                    m[i][0]=0;
+                }
+            }
+        }
+        for(int i=1;i<r;i++){
+            if(m[i][0]==0){
                 for(int j=0;j<c;j++){
                     m[i][j]=0;
                 }
             }
         }
-        for(int j=0;j<c;j++){
-            if(pcol[j]){
+        for(int j=1;j<c;j++){
+            if(m[0][j]==0){
                 for(int i=0;i<r;i++){
                     m[i][j]=0;
                 }
             }
         }
-        
+        if(rowz){
+            for(int j=0;j<c;j++){
+                m[0][j]=0;
+            }
+        }
+        if(colz){
+            for(int i=0;i<r;i++){
+                m[i][0]=0;
+            }
+        }
     }
 };
