ef16f4a3c84012f30aeea20583abbf991e3d9b4c
Pratyaksh <90966241+Pratyaksh0610@users.noreply.github.com>
2024-05-25 23:06:33 +0530

    Time: 1190 ms (5.18%) | Memory: 485.8 MB (5.01%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -5,32 +5,44 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, ll t) {
         ll n=nums.size();
         vector<vector<int>>ans;
-        set<vector<int>>s;
+        // set<vector<int>>s;
         sort(nums.begin(),nums.end());
 
         for(int i=0;i<n;i++){
+            if(i!=0&&nums[i]==nums[i-1]){
+                continue;
+            }
             for(int j=i+1;j<n;j++){
-                if(j!=i+1&&nums[j]==nums[i]){
+                if(j!=i+1&&nums[j]==nums[j-1]){
                     continue;
                 }
                 ll sum=nums[i]+nums[j];
-                unordered_map<ll,ll>m;
+                unordered_map<ll,int>m;
                 for(int k=j+1;k<n;k++){
                     ll tot=sum+nums[k];
                     ll rem=t-tot;
                     if(m.find(rem)!=m.end()){
-                        // ans++;
                         vector<int>temp={nums[i],nums[j],nums[k],(int)rem};
-                        // ans.push_back(temp);
-                        s.insert(temp);
+                        ans.push_back(temp);
+                        int value=nums[k];
+                        while(k<n&&nums[k]==value){
+                            k++;
+                        }
+                        if(k<n){
+                            k--;
+                        }
+                        // s.insert(temp);
                     }
-                    m[nums[k]]++;
+                    if(k<n){
+                        m[nums[k]]++;
+                    }
+                    // m[nums[k]]++;
                 }
             }
         }
-        for(auto it:s){
-            ans.push_back(it);
-        }
+        // for(auto it:s){
+        //     ans.push_back(it);
+        // }
         return ans;
     }
 };
