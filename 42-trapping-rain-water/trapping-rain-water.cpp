a540a1f0574a06188b4f3c1f1b58b998078104c5
Pratyaksh <90966241+Pratyaksh0610@users.noreply.github.com>
2024-06-16 19:50:05 +0530

    Time: 11 ms (53.87%) | Memory: 22.4 MB (63.34%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -2,48 +2,27 @@ class Solution {
 public:
     int trap(vector<int>& h) {
         int n=h.size();
-        // stack<int>st;
-        vector<int>nge(n,n),pge(n,-1);
+        int l=0,r=n-1,leftMax=h[l],rightMax=h[r];
         int ans=0;
-        int maxi=INT_MIN;
-        for(int i=0;i<n;i++){
-            // while(st.size()&&h[st.top()]<h[i]){
-            //     int ind=st.top();
-            //     st.pop();
-            //     nge[ind]=i;
-            // }
-            // st.push(i);
-            maxi=max(maxi,h[i]);
-            nge[i]=maxi;
-        }
-        maxi=INT_MIN;
-        // while(st.size()) st.pop();
-        for(int i=n-1;i>=0;i--){
-            // while(st.size()&&h[st.top()]<h[i]){
-            //     int ind=st.top();
-            //     st.pop();
-            //     pge[ind]=i;
-            // }
-            // // if(st.size(
-            // st.push(i);
-            maxi=max(maxi,h[i]);
-            pge[i]=maxi;
-        }
-        // for(auto it:nge){
-        //     cout<<it<<" ";
-        // }
-        // cout<<endl;
-        //  for(auto it:pge){
-        //     cout<<it<<" ";
-        // }
-        // cout<<endl;
-        for(int i=0;i<n;i++){
-            // if(nge[i]==n||pge[i]==-1){
-            //     continue;
-            // }
-            int add=min(nge[i],pge[i])-h[i];
-            // cout<<i<<" "<<add<<endl;
-            ans+=add;
+        while(l<=r){
+            if(h[l]<=h[r]){
+                if(h[l]>=leftMax){
+                    leftMax=h[l];
+                }
+                else{
+                    ans+=(leftMax-h[l]);
+                }
+                    l++;
+            }
+            else if(h[l]>h[r]){
+                if(h[r]>rightMax){
+                    rightMax=h[r];
+                }
+                else{
+                    ans+=rightMax-h[r];
+                }
+                r--;
+            }
         }
         return ans;
     }
