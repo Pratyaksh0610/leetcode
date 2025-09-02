54697264eea2762718b583472c2c61bf91b4539c
Pratyaksh <90966241+Pratyaksh0610@users.noreply.github.com>
2025-09-02 23:51:04 +0530

    Time: 27 ms (22.22%) | Memory: 41.5 MB (16.30%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -1,29 +1,27 @@
 class Solution {
+    static bool cmp(vector<int> a,vector<int> b){
+        if(a[0]<b[0]) return true;
+        else if(a[0]>b[0]) return false;
+        else if(a[1]>b[1]){
+            return true;
+        }
+        return false;
+    }
 public:
     int numberOfPairs(vector<vector<int>>& p) {
         int n=p.size(),cnt=0;
+        sort(p.begin(),p.end(),cmp);
         for(int i=0;i<n;i++){
-            int tlx=p[i][0],tly=p[i][1];
-            for(int j=0;j<n;j++){
-                int brx=p[j][0],bry=p[j][1];
-                if(tlx>brx||tly<bry||i==j){
-                    continue;
-                }
-                bool isPossible=true;
-                int trx=brx,tryy=tly;
-                int blx=tlx,bly=bry;
-                for(int k=0;k<n;k++){
-                    if(k==i||k==j){
-                        continue;
-                    }
-                    int px=p[k][0],py=p[k][1];
-                    if(px>=tlx&&px<=trx&&py>=bly&&py<=tryy){
-                        isPossible=false;
-                        break;
-                    }
-                }
-                if(isPossible){
+            int upperY=p[i][1];
+            int lowerY=INT_MIN;
+            for(int j=i+1;j<n;j++){
+                int currentY=p[j][1];
+                if(currentY<=upperY&&currentY>lowerY){
                     cnt++;
+                    lowerY=currentY;
+                }
+                if(currentY==upperY){
+                    break;
                 }
             }
         }
