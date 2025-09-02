479fa369b2272d0287b4d7b6ec5b60ee35bbac4b
Pratyaksh <90966241+Pratyaksh0610@users.noreply.github.com>
2025-09-02 23:30:27 +0530

    Time: 17 ms (31.85%) | Memory: 32.7 MB (39.26%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& p) {
-        // sort(p.begin(),p.end());
-        // vector<bool>valid(n,true);
         int n=p.size(),cnt=0;
         for(int i=0;i<n;i++){
-            //top left
             int tlx=p[i][0],tly=p[i][1];
             for(int j=0;j<n;j++){
-                //bottom right
                 int brx=p[j][0],bry=p[j][1];
                 if(tlx>brx||tly<bry||i==j){
                     continue;
@@ -21,10 +17,6 @@ public:
                         continue;
                     }
                     int px=p[k][0],py=p[k][1];
-                    // cout<<"POINT"<<endl;
-                    // cout<<px<<" "<<py<<endl;
-                    // cout<<tlx<<" "<<trx<<endl;
-                    // cout<<bly<<" "<<tryy<<endl;
                     if(px>=tlx&&px<=trx&&py>=bly&&py<=tryy){
                         isPossible=false;
                         break;
@@ -32,7 +24,6 @@ public:
                 }
                 if(isPossible){
                     cnt++;
-                    cout<<i<<" "<<j<<" BNGYA";
                 }
             }
         }
