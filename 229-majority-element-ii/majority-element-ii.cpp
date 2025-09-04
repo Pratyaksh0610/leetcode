952019ef9625ce8aff1ca7abc224d1c077cbb865
Pratyaksh <90966241+Pratyaksh0610@users.noreply.github.com>
2025-09-05 01:01:31 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (81.31%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,56 +1,42 @@
 class Solution {
+    //
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        vector<int>ans;
         int n=nums.size();
-        int el1=INT_MIN,el2=INT_MIN;
-        int c1=0,c2=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==el1){
-                c1++;
+        int limit=n/3;
+        int n1=nums[0],n2=INT_MIN;
+        int cnt1=1,cnt2=0;
+        for(int i=1;i<n;i++){
+            if(nums[i]==n1){
+                cnt1++;
             }
-            else if(nums[i]==el2){
-                c2++;
+            else if(nums[i]==n2){
+                cnt2++;
             }
-            else if(el1==INT_MIN){
-                el1=nums[i];
-                c1=1;
+            else if(cnt1==0){
+                n1=nums[i];
+                cnt1=1;
             }
-            else if(el2==INT_MIN){
-                el2=nums[i];
-                c2=1;
+            else if(cnt2==0){
+                n2=nums[i];
+                cnt2=1;
             }
             else{
-                // c1--;
-                // c2--;
-                if(c1==0){
-                    el1=nums[i];
-                    c1=1;
-                }
-                else if(c2==0){
-                    el2=nums[i];
-                    c2=1;
-                }
-                else{
-                    c1--;
-                    c2--;
-                }
+                cnt1--;
+                cnt2--;
             }
         }
-        c1=0,c2=0;
+        cnt1=0,cnt2=0;
         for(int i=0;i<n;i++){
-            if(nums[i]==el1){
-                c1++;
-            }
-            else if(nums[i]==el2){
-                c2++;
-            }
+            if(nums[i]==n1) cnt1++;
+            if(nums[i]==n2) cnt2++;
         }
-        vector<int>ans;
-        if(c1>n/3){
-            ans.push_back(el1);
+        if(cnt1>limit){
+            ans.push_back(n1);
         }
-        if(c2>n/3){
-            ans.push_back(el2);
+        if(cnt2>limit){
+            ans.push_back(n2);
         }
         return ans;
     }
