00abbfd5238519d170af61e9db22c1f21fd4654e
Pratyaksh <90966241+Pratyaksh0610@users.noreply.github.com>
2025-09-06 16:25:21 +0530

    Time: 546 ms (29.52%) | Memory: 240.2 MB (60.92%) - LeetSync

--- 493-reverse-pairs/reverse-pairs.cpp
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -7,8 +7,19 @@ class Solution {
         int mid=si+(ei-si)/2;
         int index=mid+1;
         for(int i=si;i<=mid;i++){
-            while(index<=ei && nums[i]>(ll)2*nums[index]){
-                index++;
+            // while(index<=ei && nums[i]>(ll)2*nums[index]){
+            //     index++;
+            // }
+            int st=index,end=ei;
+            while(st<=end){
+                int mm=st+(end-st)/2;
+                if(nums[i]>(ll)2*nums[mm]){
+                    st=mm+1;
+                    index=max(index,st);
+                }
+                else{
+                    end=mm-1;
+                }
             }
             ans+=index-(mid+1);
         }
