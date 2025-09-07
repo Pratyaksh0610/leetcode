b5bb4f3940d6438d3658f140d046ad29a87ab85f
Pratyaksh <90966241+Pratyaksh0610@users.noreply.github.com>
2025-09-07 14:42:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (92.20%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,17 +1,13 @@
 #define ll long long int
 class Solution {
-    // int fun(int i,int j)
 public:
     int uniquePaths(int m, int n) {
         int tot=m+n-2;
-        int d=m-1,r=n-1;
-        //val=fact(tot)/(fact(d)*fact(r));
-        //min(d,r);
-        int mini=min(d,r);
-        ll val=1;
+        int mini=min(m,n)-1;
+        ll ans=1;
         for(int i=0;i<mini;i++){
-            val=(val*(tot-i))/(i+1);
+            ans=(ans*(tot-i))/(i+1);
         }
-        return val;
+        return ans;
     }
 };
