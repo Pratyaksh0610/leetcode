dfb0c07dcfac27d84fce77c2303815c0c0a76fac
Pratyaksh <90966241+Pratyaksh0610@users.noreply.github.com>
2025-09-14 13:54:29 +0530

    Time: 3 ms (34.74%) | Memory: 17.4 MB (7.53%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,27 +1,30 @@
 class Solution {
-    private:
-    void fun(vector<vector<int>>&ans,vector<int>&nums,int ind,vector<int>temp){
-        if(ind>=nums.size()){
-            ans.push_back(temp);
+    void generateSubsets(vector<int>temp,vector<int>&nums,int index,set<vector<int>>&s){
+        if((index)==nums.size()){
+            s.insert(temp);
             return;
         }
-        for(int i=ind;i<nums.size();i++){
-            if(i!=ind&&nums[i]==nums[i-1]){
-                continue;
-            }
-            temp.push_back(nums[i]);
-            fun(ans,nums,i+1,temp);
-            temp.pop_back();
-        }
-        ans.push_back(temp);
+        //ntake
+        int val=nums[index];
+        generateSubsets(temp,nums,index+1,s);
+        //take
+        temp.push_back(val);
+        generateSubsets(temp,nums,index+1,s);
+        return;
     }
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        int n=nums.size();
+        set<vector<int>>s;
         sort(nums.begin(),nums.end());
+        // for(auto it:nums){
+        //     s.insert(it);
+        // }
         vector<vector<int>>ans;
         vector<int>temp;
-        fun(ans,nums,0,temp);
+        generateSubsets(temp,nums,0,s);
+        for(auto it:s){
+            ans.push_back(it);
+        }
         return ans;
     }
 };
