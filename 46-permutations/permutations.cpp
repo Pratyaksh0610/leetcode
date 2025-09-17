0660e1fa3f74529faffd8f63d9a53fdecaead987
Pratyaksh <90966241+Pratyaksh0610@users.noreply.github.com>
2025-09-17 23:48:37 +0530

    Time: 8 ms (6.00%) | Memory: 15.2 MB (5.19%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,22 +1,47 @@
 class Solution {
-    void fun(vector<vector<int>>&ans,vector<int>&nums,int ind,vector<int>temp){
-        if(ind>=nums.size()){
-            ans.push_back(temp);
+    private:
+    void fun(set<vector<int>>&ans,deque<int>&dq,vector<int>&vis,vector<int>&nums){
+        if(dq.size()==nums.size()){
+            vector<int>temp;
+            for(auto it:dq){
+                temp.push_back(it);
+            }
+            ans.insert(temp);
             return;
         }
-        temp.push_back(nums[ind]);
-        for(int i=0;i<temp.size();i++){
-            swap(temp[i],temp[temp.size()-1]);
-            fun(ans,nums,ind+1,temp);
-            swap(temp[i],temp[temp.size()-1]);
+        bool added=false;
+        for(int i=0;i<vis.size();i++){
+            if(vis[i]==-1){
+                dq.push_front(nums[i]);
+                vis[i]=1;
+                fun(ans,dq,vis,nums);
+                dq.pop_front();
+                dq.push_back(nums[i]);
+                fun(ans,dq,vis,nums);
+                dq.pop_back();
+                vis[i]=-1;
+            }
         }
         return;
     }
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>ans;
-        vector<int>temp;
-        fun(ans,nums,0,temp);
-        return ans;
+        // vector<int>temp = {nums[0]};
+        set<vector<int>>ans;
+        vector<int>vis(nums.size(),-1);
+        vis[0]=1;
+        // unordered_map<int,int>m;
+        deque<int>dq;
+        dq.push_front(nums[0]);
+        // for(int i=1;i<nums.size();i++){
+        //     m[nums[i]]++;
+        // }
+        // m[nums[0]]=true;
+        fun(ans,dq,vis,nums);
+        vector<vector<int>>fans;
+        for(auto it:ans){
+            fans.push_back(it);
+        }
+        return fans;
     }
 };
